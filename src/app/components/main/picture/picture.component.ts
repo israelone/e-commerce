e88b12f3377355fcd840206b8aaa2d7e49de292d
IzzyDev <israel.lopez.dev@gmail.com>
2022-01-11 00:42:41 -0600

    Added elements and some styling

--- src/app/components/main/picture/picture.component.ts
+++ src/app/components/main/picture/picture.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-picture',
   templateUrl: './picture.component.html',
-  styleUrls: ['./picture.component.scss']
+  styleUrls: ['./picture.component.scss'],
 })
 export class PictureComponent implements OnInit {
+  imageSrc: string = `../../../../assets/images/image-product-1.jpg`;
+  imageSelected: number = 1;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  changeImage(e: any) {
+    this.imageSelected = e.target.id;
+    this.imageSrc = `../../../../assets/images/image-product-${e.target.id}.jpg`;
+    console.log(this.imageSelected);
   }
-
 }
