fa3788541ac4e69736878a585197217596bf2d7e
IzzyDev <israel.lopez.dev@gmail.com>
2022-01-10 23:50:31 -0600

    Added first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './components/main/main.component';
+import { DescriptionComponent } from './components/main/description/description.component';
+import { PictureComponent } from './components/main/picture/picture.component';
+import { NavigationComponent } from './components/main/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    DescriptionComponent,
+    PictureComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
