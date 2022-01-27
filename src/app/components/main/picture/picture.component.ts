73b4cf0c7753d2693e772ff11e05af5ea1e247ad
IzzyDev <israel.lopez.dev@gmail.com>
2022-01-27 07:34:46 -0600

    Added classes to div, modal toggle and styling

--- src/app/components/main/picture/picture.component.ts
+++ src/app/components/main/picture/picture.component.ts
@@ -8,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class PictureComponent implements OnInit {
   imageSrc: string = `../../../../assets/images/image-product-1.jpg`;
   imageSelected: number = 1;
+  showModal:boolean = false
   constructor() {}
 
   ngOnInit(): void {}
 
+  toggleModal(){
+    if(this.showModal){
+      this.showModal=false;
+    }else{
+      this.showModal=true;
+    }
+  }
+
   changeImage(e: any) {
     this.imageSelected = e.target.id;
     this.imageSrc = `../../../../assets/images/image-product-${e.target.id}.jpg`;
-    console.log(this.imageSelected);
   }
 }
