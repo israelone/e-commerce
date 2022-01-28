0638f38887c6628f36e15b6d7b869521fa4a6bae
IzzyDev <israel.lopez.dev@gmail.com>
2022-01-28 07:50:51 -0600

    Added functionality for quantity

--- src/app/components/main/description/description.component.ts
+++ src/app/components/main/description/description.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./description.component.scss']
 })
 export class DescriptionComponent implements OnInit {
-
+  quantity:number=0;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addOneProduct(){
+    this.quantity+=1;
+  }
+
+  substractOneProduct(){
+    this.quantity-=1;
   }
 
 }
