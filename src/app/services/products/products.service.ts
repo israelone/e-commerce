3f0746497561b149a1692c49de7593226c96af52
IzzyDev <itzisraelone@gmail.com>
2022-02-02 16:01:54 -0600

    Added more images to the products

--- src/app/services/products/products.service.ts
+++ src/app/services/products/products.service.ts
@@ -11,15 +11,28 @@ export class ProductsService {
         "These low-profile sneakers are your perfect casual wear companion. Featuring a durable subber outer sole, they'll withstand everything the weather an offer",
       price: 250,
       discount: 50,
-      mainImage: {
+      quantity: 0,
+      mainImages: [{
         src: 'assets/images/image-product-1.jpg',
         alt: '',
       },
+      {
+        src: 'assets/images/image-product-2.jpg',
+        alt: '',
+      },
+      {
+        src: 'assets/images/image-product-3.jpg',
+        alt: '',
+      },
+      {
+        src: 'assets/images/image-product-4.jpg',
+        alt: '',
+      }],
       thumbnails: [
-        { image: 'assets/images/image-product-1-thumbnail.jpg', alt: '', id:"1" },
-        { image: 'assets/images/image-product-2-thumbnail.jpg', alt: '', id:"2" },
-        { image: 'assets/images/image-product-3-thumbnail.jpg', alt: '', id:"3" },
-        { image: 'assets/images/image-product-4-thumbnail.jpg', alt: '', id:"4" },
+        { image: 'assets/images/image-product-1-thumbnail.jpg', alt: '', id:"0" },
+        { image: 'assets/images/image-product-2-thumbnail.jpg', alt: '', id:"1" },
+        { image: 'assets/images/image-product-3-thumbnail.jpg', alt: '', id:"2" },
+        { image: 'assets/images/image-product-4-thumbnail.jpg', alt: '', id:"3" },
       ],
     },
   ];
