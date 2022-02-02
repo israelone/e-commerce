04b4e91312ece5ddfce0302e98a9a01aef04f72d
IzzyDev <itzisraelone@gmail.com>
2022-02-02 16:18:17 -0600

    Used input recevied from parent component

--- src/app/components/main/navigation/shopping-cart-modal/cart-item/cart-item.component.ts
+++ src/app/components/main/navigation/shopping-cart-modal/cart-item/cart-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cart-item',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent implements OnInit {
-
+  @Input() product: any;
   constructor() { }
-
   ngOnInit(): void {
+
   }
 
 }
