8de2f2f0685e7297f761ebc09a5a355f83be6694
IzzyDev <itzisraelone@gmail.com>
2022-02-10 14:13:18 -0600

    Added mobile buttons to change image

--- src/app/components/main/picture/picture.component.ts
+++ src/app/components/main/picture/picture.component.ts
@@ -7,28 +7,57 @@ import { ProductsService } from 'src/app/services/products/products.service';
   styleUrls: ['./picture.component.scss'],
 })
 export class PictureComponent implements OnInit {
-  @Input() currentProduct!:Product;
-  mainImageSrc: string = "";
+  @Input() currentProduct!: Product;
+  mainImageSrc: string = '';
   imageSelected: number = 1;
-  showModal:boolean = false
-  products:Product[];
+  showModal: boolean = false;
+  products: Product[];
+  mobile!: boolean;
 
-  constructor(private productsService : ProductsService) {
-    this.products=this.productsService.getProducts();
+  constructor(private productsService: ProductsService) {
+    this.products = this.productsService.getProducts();
   }
 
-  ngOnInit(): void {this.mainImageSrc = this.currentProduct.mainImages[0].src;}
+  ngOnInit(): void {
+    this.mainImageSrc = this.currentProduct.mainImages[0].src;
+    if (window.screen.width === 375 || window.screen.width < 375) {
+      this.mobile = true;
+    }
+  }
 
-  toggleModal(){
-    if(this.showModal){
-      this.showModal=false;
-    }else{
-      this.showModal=true;
+  toggleModal() {
+    if (!this.mobile) {
+      if (this.showModal) {
+        this.showModal = false;
+      } else {
+        this.showModal = true;
+      }
     }
   }
 
   changeImage(e: any) {
     this.imageSelected = e.target.id;
-    this.mainImageSrc = this.currentProduct.mainImages[this.imageSelected - 1].src;
+    this.mainImageSrc =
+      this.currentProduct.mainImages[this.imageSelected - 1].src;
+  }
+
+  selectNextImage() {
+    if (this.imageSelected === 4) {
+      this.imageSelected = 1;
+    } else {
+      this.imageSelected += 1;
+    }
+    this.mainImageSrc =
+      this.currentProduct.mainImages[this.imageSelected - 1].src;
+  }
+
+  selectPreviousImage() {
+    if (this.imageSelected === 1) {
+      this.imageSelected = 4;
+    } else {
+      this.imageSelected -= 1;
+    }
+    this.mainImageSrc =
+      this.currentProduct.mainImages[this.imageSelected - 1].src;
   }
 }
